e5f39a1e0365fb8bf0edbe9b7ec0b3d32d9c5869
Agis <agis.septiyanto@gmail.com>
2021-04-28 21:00:58 +0700

    remove unused file

--- js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const submitForm /* HTMLFormElement */ = document.getElementById("form");
-
-    submitForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        addTodo();
-    });
-});
